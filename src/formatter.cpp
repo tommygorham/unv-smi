5a056d8089a148c104bb0f3f415e5219c2371353
Tommy Gorham <38857089+tommygorham@users.noreply.github.com>
2022-08-03 18:21:21 -0400

    format formatter.cpp haha

--- src/formatter.cpp
+++ src/formatter.cpp
@@ -4,15 +4,15 @@
 // count whitespace  
 int countws(const std::string &s){ 
   int spaces =  std::count_if(s.begin(), s.end(),
-	              [](unsigned char c){ return std::isspace(c); });
+	                      [](unsigned char c){ return std::isspace(c); });
     return spaces; 
 } 
 
 // clean up whitespace
 std::string sanitize(const std::string &s){ 
-	std::string output = ""; 
+    std::string output = ""; 
     output.clear();  // Remove this to append to an existing string
     std::unique_copy (s.begin(), s.end(), std::back_insert_iterator<std::string>(output),
-                                     [](char a,char b){ return isspace(a) && isspace(b);});   
+                      [](char a,char b){ return isspace(a) && isspace(b);});   
     return output; 
 }
