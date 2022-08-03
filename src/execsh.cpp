6a9d6c7082629e19127c29782d2ab3b227db3ed1
tommygorham <tommy.gorham97@gmail.com>
2022-08-02 21:45:08 -0400

    Add Crucial detail for Windows

--- src/execsh.cpp
+++ src/execsh.cpp
@@ -18,5 +18,19 @@ std::string execsh(const char* cmd)  {
 }
 #endif
 
+#ifdef _WIN32    
+std::string execsh(const char* cmd)  { 
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(_popen(cmd, "r"), _pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
+#endif
 
 
